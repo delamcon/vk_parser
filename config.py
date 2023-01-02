8d5d8201d466da28b02489fd5684f30dc85b5d1a
delamcon <rinatt116@gmail.com>
2023-01-02 18:49:53 +0300

    add config file, enviroment and app architecture

---
+++ config.py
@@ -0,0 +1,6 @@
+vk_api_key = "..."
+
+mongo_host = '127.0.0.1'  # enter your mongodb host
+mongo_port = 27017  # enter your mongodb port
+mongo_user = '...'  # enter mongodb username for db
+mongo_user_pwd = '...'  # enter pass to username in db
